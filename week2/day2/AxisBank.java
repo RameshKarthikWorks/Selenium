6b9e71a93d600c9084b96431d45f107acd8d67c3
RameshKarthikWorks <103556134+RameshKarthikWorks@users.noreply.github.com>
2023-05-30 17:18:45 +0530

    Add files via upload

---
+++ week2/day2/AxisBank.java
@@ -0,0 +1,21 @@
+package week2.day2;
+
+public class AxisBank extends BankInfo {
+	
+	
+	public  void deposit() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I have Saving and fixed Account");
+
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		BankInfo axisBank = new AxisBank();
+		
+		axisBank.deposit();
+	}
+
+}
---
+++ week2/day2/BankInfo.java
@@ -0,0 +1,30 @@
+package week2.day2;
+
+public class BankInfo {
+
+	
+	public void saving() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I have Saving Account");
+
+	};
+	
+	public void fixed() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I have fixed Account");
+
+	};
+	
+	public void deposit() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I have deposit Account");
+
+	}
+	
+	
+	
+
+}
