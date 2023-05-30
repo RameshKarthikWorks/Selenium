ae64d627b9df6d713b629b7ea696fb3f8204cda6
RameshKarthikWorks <103556134+RameshKarthikWorks@users.noreply.github.com>
2023-05-30 16:43:38 +0530

    week1.day1
    week1.day1

---
+++ day1/Smartphone.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Smartphone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Iphone iphone = new Iphone();
+		iphone.makeCall();
+		iphone.model();
+
+	}
+
+}
